0e953e52cd01018e27e5e3c894a318f037910349
Diego Parente <diego.s.parente@gmail.com>
2021-02-28 02:09:22 -0300

    NLW#4 | aula04

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
+import { ChallengeBox } from './ChallengeBox'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/diegosparente.png" alt="github - Diego" />
@@ -8,8 +13,8 @@ export function Profile() {
         <strong>Diego Parente</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-                    Level 1
-                </p>
+            Level {level}
+        </p>
       </div>
     </div>
   )
